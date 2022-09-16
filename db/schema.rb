1bda2e596140ff3f6159e76dba2985567db35c89
Ubuntu <ubuntu@ip-172-31-10-29.eu-west-2.compute.internal>
2022-09-16 14:44:21 +0000

    Add description attribute & run second migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_16_142021) do
+ActiveRecord::Schema.define(version: 2022_09_16_142751) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
 end
